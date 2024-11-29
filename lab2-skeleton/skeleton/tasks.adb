00a22a61c8ca2276c75dfad33021720ff7a824fa
Darius Loga <dariusloga09@gmail.com>
2024-11-29 21:59:47 +0100

    refactor

--- lab2-skeleton/skeleton/tasks.adb
+++ lab2-skeleton/skeleton/tasks.adb
@@ -10,15 +10,16 @@ package body Tasks is
   -------------   
   task HelloworldTask is
     -- define its priority higher than the main procedure --
+    pragma Priority(2);
     -- main procedure priority is declared at main.adb:9
   end HelloworldTask;
 
   task body HelloworldTask is
     Next_Time : Time := Time_Zero;
+    MOTORSPEED : constant Integer := 200;
 
   begin      
     -- task body starts here ---
-
     loop
       -- read sensors and print ----
       Ada.Text_IO.Put_Line("LS1      : " & read_light_sensor(LS1)'Image);
@@ -30,6 +31,9 @@ package body Tasks is
       Ada.Text_IO.Put_Line("RIGHT    : " & button_pressed(RightButton)'Image);
       Ada.Text_IO.Put_Line("DISTANCE : " & read_distance_sensor'Image);
       Ada.Text_IO.Put_Line("-----------------------------------");
+
+      set_motor_speed(LeftMotor, MOTORSPEED); -- Could be adjusted between [-999, +999]
+      set_motor_speed(RightMotor, MOTORSPEED);
 
       Next_Time := Next_Time + Period_Display;
       delay until Next_Time;
